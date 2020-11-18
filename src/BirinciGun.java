61482fbc78468a5bbbe389f4c63afe76ab67a1e9
Refik <rgulak48@gmail.com>
2020-11-18 21:29:20 +0200

    feature

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,7 +3,14 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 		System.out.println("11:57 - Birinci Gun");
-     System.out.println("Ikinci Commit");
+        System.out.println("Ikinci Commit");
+        System.out.println("10:26 - feature branch");
+     
+     
+     
+     
+     
+     
 	}
 
 }
