d8d7302375cd060542df4074ab1754e28016349b
Refik <rgulak48@gmail.com>
2020-11-18 22:11:02 +0200

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,8 +5,8 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gun");
         System.out.println("Ikinci Commit");
         System.out.println("10:26 - feature branch");
-     
-     
+        System.out.println("11:06 - satir ekleme");
+        System.out.println("11:08 - satir ekleme2");
      
      
      
