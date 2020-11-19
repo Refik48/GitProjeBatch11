d01164e404f60f945351d16640ec1a9a04ece3c0
Refik48 <74262442+Refik48@users.noreply.github.com>
2020-11-19 10:09:08 +0200

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -9,7 +9,7 @@ public class BirinciGun {
         System.out.println("11:08 - satir ekleme2");
         System.out.println("10:11 - Ikinci Gun");
         System.out.println("10:26 - Ikinci Gun");
-     
+        System.out.println("10:35 - Ikinci Gun");
      
 	}
 
